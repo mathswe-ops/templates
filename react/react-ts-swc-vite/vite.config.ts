1bf6ab0925fa77043127c319db9388f61dfba142
Tobias Briones <tbriones@mathswe.com>
2024-09-22 22:36:43 -0600

    Integrate dev dep vite-plugin-eslint
    It makes vite run eslint when running the application in development.

--- react/react-ts-swc-vite/vite.config.ts
+++ react/react-ts-swc-vite/vite.config.ts
@@ -1,10 +1,16 @@
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
 import { resolve } from "node:path";
+import { defineConfig } from "vite";
+// @ts-expect-error They must fix this dev dep to support its types correctly
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [ react() ],
+    plugins: [
+        react(),
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        eslint(),
+    ],
     resolve: {
         alias: {
             "@": resolve(__dirname, "src"),
