d41ef6088f8b70d7c9d56a0ced1810f89bcbc636
Tobias Briones <tbriones@mathswe.com>
2024-09-22 21:57:51 -0600

    Expand eslint type awareness according to default vite README.md steps

--- react/react-ts-swc-vite/src/main.tsx
+++ react/react-ts-swc-vite/src/main.tsx
@@ -3,9 +3,20 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!)
-    .render(
-        <StrictMode>
-            <App />
-        </StrictMode>,
-    );
+const rootEl = document.getElementById("root");
+
+if (rootEl === null) {
+    console.error("Root element not found");
+}
+else {
+    render(rootEl);
+}
+
+function render(rootEl: HTMLElement) {
+    createRoot(rootEl)
+        .render(
+            <StrictMode>
+                <App />
+            </StrictMode>,
+        );
+}
